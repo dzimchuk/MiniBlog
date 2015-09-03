101b308bda9664487d96c3f55032d6b67287f4c6
Andrei Dzimchuk <andrei@dzimchuk.net>
2015-09-03 18:53:34 +0300

    extracted search interface

---
+++ MiniBlog.Contracts/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.Azure.Search.Models;
+
+namespace MiniBlog.Contracts
+{
+    public interface ISearchService
+    {
+        IList<SearchResult> Search(string searchText);
+        IList<SuggestResult> Suggest(string searchText);
+    }
+}
