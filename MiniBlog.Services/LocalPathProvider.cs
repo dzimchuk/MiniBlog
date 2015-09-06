c41f712b7306345b68cad72d37cb02455a30b901
Andrei Dzimchuk <andrei@dzimchuk.net>
2015-09-06 19:09:04 +0300

    updated local path provider so it works for the web app

--- MiniBlog.Services/LocalPathProvider.cs
+++ MiniBlog.Services/LocalPathProvider.cs
@@ -17,15 +17,16 @@ namespace MiniBlog.Services
 
         public string GetAppDataPath()
         {
-#if DEBUG
             return Path.Combine(configuration.Find("WebRootPath"), AppData);
-#else
-            var webRoot = Environment.GetEnvironmentVariable("WEBROOT_PATH");
-            if (string.IsNullOrWhiteSpace(webRoot))
-                throw new Exception("WEBROOT_PATH not defined");
-
-            return Path.Combine(webRoot, AppData);
-#endif
+            //#if DEBUG
+            //            return Path.Combine(configuration.Find("WebRootPath"), AppData);
+            //#else
+            //            var webRoot = Environment.GetEnvironmentVariable("WEBROOT_PATH");
+            //            if (string.IsNullOrWhiteSpace(webRoot))
+            //                throw new Exception("WEBROOT_PATH not defined");
+            //
+            //            return Path.Combine(webRoot, AppData);
+            //#endif
         }
     }
 }
