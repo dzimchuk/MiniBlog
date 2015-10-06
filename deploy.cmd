6e446af2b6ac55bd664d633a238757dbbca5f27b
Andrei Dzimchuk <andrei@dzimchuk.net>
2015-10-06 22:42:53 +0300

    updated deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -67,6 +67,7 @@ SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 
 echo Handling .NET Web Site deployment.
 
+:: 1. Restore NuGet packages
 IF /I "MiniBlog.sln" NEQ "" (
   call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\MiniBlog.sln"
   IF !ERRORLEVEL! NEQ 0 goto error
@@ -75,13 +76,32 @@ IF /I "MiniBlog.sln" NEQ "" (
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-:: 1. Build to the repository path
-call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\MiniBlog.sln" /verbosity:m /nologo %SCM_BUILD_ARGS%
+:: 2. Build and package
+call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\MiniBlog.sln" /p:Configuration=Release /verbosity:m /nologo %SCM_BUILD_ARGS%
 IF !ERRORLEVEL! NEQ 0 goto error
+
+call :BuildWebJob MiniBlog.Backup MiniBlog.Backup.csproj
+IF !ERRORLEVEL! NEQ 0 goto error
+
+call :BuildWebJob MiniBlog.BlogIndexer MiniBlog.BlogIndexer.csproj
+IF !ERRORLEVEL! NEQ 0 goto error
+
+call :BuildWebJob MiniBlog.ImageOptimizer MiniBlog.ImageOptimizer.csproj
+IF !ERRORLEVEL! NEQ 0 goto error
+rd /s /q "%DEPLOYMENT_TEMP%\Resources"
+
+call :BuildWebJob MiniBlog.PostIndexer MiniBlog.PostIndexer.csproj
+IF !ERRORLEVEL! NEQ 0 goto error
+
+call :BuildWebJob MiniBlog.PostSync MiniBlog.PostSync.csproj
+IF !ERRORLEVEL! NEQ 0 goto error
+
+robocopy "%DEPLOYMENT_SOURCE%\PrecompiledWeb\localhost_36123" "%DEPLOYMENT_TEMP%" /E
+IF !ERRORLEVEL! GTR 3 goto error
 
 :: 2. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\Website" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;app_data\posts"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
@@ -92,6 +112,18 @@ IF DEFINED POST_DEPLOYMENT_ACTION call "%POST_DEPLOYMENT_ACTION%"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 goto end
+
+:BuildWebJob
+set _folder_=%1
+set _project_=%2
+call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\%_folder_%\%_project_%" /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%\%_folder_%";Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\"
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+robocopy "%DEPLOYMENT_TEMP%\%_folder_%" "%DEPLOYMENT_TEMP%" /E /XD bin
+IF %ERRORLEVEL% LEQ 3 (
+	rd /s /q "%DEPLOYMENT_TEMP%\%_folder_%"
+	exit /b 0
+)
+exit /b %ERRORLEVEL%
 
 :: Execute command routine that will echo out when error
 :ExecuteCmd
