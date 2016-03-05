191a4b75810b164c2ff6d388c3cc666e10de8c64
Andrei Dzimchuk <andrei@dzimchuk.net>
2016-03-05 16:02:37 +0300

    do not try to strip html tags before indexing posts any more

--- MiniBlog.Search/Extensions/PostExtensions.cs
+++ MiniBlog.Search/Extensions/PostExtensions.cs
@@ -1,20 +1,17 @@
-﻿using System.Text.RegularExpressions;
-using Microsoft.Azure.Search.Models;
+﻿using Microsoft.Azure.Search.Models;
 using MiniBlog.Contracts.Model;
 
 namespace MiniBlog.Search.Extensions
 {
     public static class PostExtensions
     {
-        private static readonly Regex RegexHtml = new Regex("<.*?>", RegexOptions.Compiled);
-
         public static Document ToDocument(this Post post)
         {
             return new Document
                    {
                        { "Id", post.Id },
                        { "Title", post.Title },
-                       { "Content", RegexHtml.Replace(post.Content, " ") },
+                       { "Content", post.Content },
                        { "Categories", post.Categories },
                        { "IsPublished", post.IsPublished },
                        { "PubDate", post.PubDate }
